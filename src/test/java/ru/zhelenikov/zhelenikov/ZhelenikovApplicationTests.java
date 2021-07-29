613a9f8a650eed819cbe22fa9f816c9f8daa504e
44nc40rus <9620976326@mail.ru>
2021-07-29 15:54:58 +0300

    authdemo

---
+++ src/test/java/ru/zhelenikov/zhelenikov/ZhelenikovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhelenikov.zhelenikov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhelenikovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
