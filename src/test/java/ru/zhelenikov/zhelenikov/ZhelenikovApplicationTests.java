02d1f1869ec8179a2d4c7a3b04427ff02a3b1242
44nc40rus <9620976326@mail.ru>
2021-07-30 05:06:31 +0300

    authdemo

--- src/test/java/ru/zhelenikov/zhelenikov/ZhelenikovApplicationTests.java
+++ src/test/java/ru/zhelenikov/zhelenikov/ZhelenikovApplicationTests.java
@@ -1,13 +1,13 @@
 package ru.zhelenikov.zhelenikov;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ZhelenikovApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
